846eb139a913b50c2ef98c0e6343298964f40659
partychickenking <partychickenking@gmail.com>
2020-06-29 09:46:35 +0200

    Added Heroku port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use('/static', express.static('static'));
 app.use(express.static('static'))
